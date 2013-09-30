b67414cb7cf16ebdd4e1f0fa63dcce7482f56fe5
Aidan <aidan.mclaughlin@students.olin.edu>
2013-09-30 17:15:49 -0400

    working toDo

--- build/source/r/debug/com/evansimpson/mobpro/lab0/R.java
+++ build/source/r/debug/com/evansimpson/mobpro/lab0/R.java
@@ -11,8 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        /**  From: file:/home/evan/MobileProto/Lab0Project/Lab0/src/main/res/values/dimens.xml 
- From: file:/home/evan/MobileProto/Lab0Project/Lab0/src/main/res/values-sw720dp-land/dimens.xml 
+        /**  From: file:/home/amclaughlin/toDo/src/main/res/values/dimens.xml 
+ From: file:/home/amclaughlin/toDo/src/main/res/values-sw720dp-land/dimens.xml 
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
@@ -21,30 +21,43 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int button_send=0x7f080000;
+        public static final int delete=0x7f080004;
+        public static final int editText=0x7f080001;
+        public static final int listItem=0x7f080003;
+        public static final int listView=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int thing_to_do=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int first=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
-        /**  From: file:/home/evan/MobileProto/Lab0Project/Lab0/src/main/res/values/strings.xml 
+        /**  From: file:/home/amclaughlin/toDo/src/main/res/values/strings.xml 
          */
         public static final int action_settings=0x7f050000;
         public static final int app_name=0x7f050001;
-        public static final int hello_world=0x7f050002;
+        public static final int button=0x7f050002;
+        public static final int delete=0x7f050003;
+        public static final int hello_world=0x7f050004;
+        public static final int placehold=0x7f050005;
+        public static final int task=0x7f050006;
     }
     public static final class style {
-        /**  From: file:/home/evan/MobileProto/Lab0Project/Lab0/src/main/res/values/styles.xml 
+        /**  From: file:/home/amclaughlin/toDo/src/main/res/values/styles.xml 
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
- From: file:/home/evan/MobileProto/Lab0Project/Lab0/src/main/res/values-v11/styles.xml 
+ From: file:/home/amclaughlin/toDo/src/main/res/values-v11/styles.xml 
  API 11 theme customizations can go here. 
+ From: file:/home/amclaughlin/toDo/src/main/res/values-v14/styles.xml 
+ API 14 theme customizations can go here. 
          */
         public static final int AppBaseTheme=0x7f060000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
--- src/main/java/com/evansimpson/mobpro/lab0/MainActivity.java
+++ src/main/java/com/evansimpson/mobpro/lab0/MainActivity.java
@@ -1,23 +1,63 @@
 package com.evansimpson.mobpro.lab0;
 
+import android.content.Context;
+import android.content.Intent;
+import android.inputmethodservice.ExtractEditText;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
+
+    private ArrayList<String> my2Do;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        ListView listView1 = (ListView) findViewById(R.id.listView);
+        my2Do = new ArrayList<String>();
+
+        final Adapter rockOn = new Adapter(this, my2Do);
+
+        ListView list = (ListView) findViewById(R.id.listView);
+        list.setAdapter(rockOn);
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        Button add = (Button) findViewById(R.id.button_send);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addTodo(view, rockOn);
+            }
+        });
+
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
     }
-    
+
+    public void addTodo(View view, Adapter rockOn) {
+        EditText newTask = (EditText) findViewById(R.id.editText);
+        String work = newTask.getText().toString();
+//        Toast.makeText(this, work, Toast.LENGTH_SHORT).show();
+
+        if (work != " ") {
+        rockOn.add(work);
+        rockOn.notifyDataSetChanged();
+        }
+    }
+
+    public void destroy(View view) {
+//        Toast.makeText(this, "destroy", Toast.LENGTH_SHORT).show();
+
+    }
 }
